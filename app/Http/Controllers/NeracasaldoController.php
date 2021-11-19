c83cbd725d7a2860b2a6f33ec811f6bada1cf2cc
Andris Dwi Martono <andrisdwimartono@gmail.com>
2021-11-19 10:35:52 +0700

    Perbaikan Fitur Report Neraca Saldo dan Nerraca
    Perbaikann fitur neraca saldo dan neraca
    
    andrisdwimartono

--- app/Http/Controllers/NeracasaldoController.php
+++ app/Http/Controllers/NeracasaldoController.php
@@ -63,8 +63,8 @@ class NeracasaldoController extends Controller
     {
         $page_data = $this->tabledesign();
         $page_data["page_method_name"] = "List";
-        $page_data["footer_js_page_specific_script"] = ["paging.page_specific_script.footer_js_list"];
-        $page_data["header_js_page_specific_script"] = ["paging.page_specific_script.header_js_list"];
+        $page_data["footer_js_page_specific_script"] = ["neracasaldo.page_specific_script.footer_js_list"];
+        $page_data["header_js_page_specific_script"] = ["neracasaldo.page_specific_script.header_js_list"];
         
         return view("neracasaldo.list", ["page_data" => $page_data]);
     }
