5aeb81023638ce65801afa8c52d35af5180e3695
Muhammad Alfian <ini.muhalfian@gmail.com>
2021-12-17 22:29:33 +0700

    update neraca

--- resources/views/coa/print.blade.php
+++ resources/views/coa/print.blade.php
@@ -84,8 +84,7 @@
                         <?php } } ?>
                     </td>
                     <td width="6em">
-                        {{$globalsetting->logo_instansi}}
-                        <img class='logo' src="{{ public_path('/logo_instansi/'.$globalsetting->logo_instansi) }}" alt="UMSIDA">
+                        <img class='logo' src="{{ asset('/logo_instansi/'.$globalsetting->logo_instansi) }}" alt="UMSIDA">
                     </td>
                 </tr>
             </table>
