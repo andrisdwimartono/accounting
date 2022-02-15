4b7e4bc210bbc1749415821e0b20d1bcae5c3646
Muhammad Alfian <ini.muhalfian@gmail.com>
2022-02-16 05:01:45 +0700

    update

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -395,7 +395,7 @@ class RoleController extends Controller
             $user_menus = User_role_menu::find(1)
             ->select(['menus.*'])
             ->leftJoin('menus','menus.id','user_role_menus.menu_id')
-            ->where("role",Auth::user()->role)->where("is_granted", "on")
+            ->where("role_label",Auth::user()->role_label)->where("is_granted", "on")
             ->where("is_shown_at_side_menu", "on")->orderBy("mp_sequence", "ASC")->orderBy("m_sequence", "ASC")
             ->get();
 
