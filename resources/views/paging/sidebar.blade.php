450c50f7e16b34612a80cca3ff48f7ef3815a8d1
Muhammad Alfian <ini.muhalfian@gmail.com>
2022-02-17 21:33:50 +0800

    update tooltips

--- resources/views/paging/sidebar.blade.php
+++ resources/views/paging/sidebar.blade.php
@@ -4,32 +4,32 @@
         <div class="deznav">
             <div class="deznav-scroll">
 				<ul class="nav menu-tabs">
-					<li class="nav-item">
+					<li class="nav-item"  data-toggle="tooltip" data-placement="right" title="Dashboard">
 						<a class="nav-link" id="caknav-dashboard" data-toggle="tab" href="#cakmenu-dashboard">
 							<svg id="icon-home1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
 						</a>
 					</li>
-					<li class="nav-item">
+					<li class="nav-item"  data-toggle="tooltip" data-placement="right" title="Master">
 						<a class="nav-link" id="caknav-master" data-toggle="tab" href="#cakmenu-master">
 							<svg id="icon-home" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-box"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
 						</a>
 					</li>
-					<li class="nav-item">
+					<li class="nav-item"  data-toggle="tooltip" data-placement="right" title="Jurnal">
 						<a class="nav-link"  id="caknav-transaksi" data-toggle="tab" href="#cakmenu-transaksi">
 							<svg id="icon-bootstrap" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>
 						</a>
 					</li>
-					<li class="nav-item">
+					<li class="nav-item"  data-toggle="tooltip" data-placement="right" title="Laporan">
 						<a class="nav-link"  id="caknav-laporan" data-toggle="tab" href="#cakmenu-laporan">
 							<svg id="icon-forms" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" style="stroke-dasharray: 66, 86; stroke-dashoffset: 0;"></path><path d="M14,2L14,8L20,8" style="stroke-dasharray: 12, 32; stroke-dashoffset: 0;"></path><path d="M16,13L8,13" style="stroke-dasharray: 8, 28; stroke-dashoffset: 0;"></path><path d="M16,17L8,17" style="stroke-dasharray: 8, 28; stroke-dashoffset: 2;"></path><path d="M10,9L9,9L8,9" style="stroke-dasharray: 2, 22; stroke-dashoffset: 12;"></path></svg>
 						</a>
 					</li>
-					<li class="nav-item">
+					<li class="nav-item"  data-toggle="tooltip" data-placement="right" title="E-budgeting">
 						<a class="nav-link"  id="caknav-ebudgeting" data-toggle="tab" href="#cakmenu-ebudgeting">
 							<svg id="icon-table" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-server"><rect x="2" y="2" width="20" height="8" rx="2" ry="2"></rect><rect x="2" y="14" width="20" height="8" rx="2" ry="2"></rect><line x1="6" y1="6" x2="6" y2="6"></line><line x1="6" y1="18" x2="6" y2="18"></line></svg>
 						</a>
 					</li>
-					<li class="nav-item">
+					<li class="nav-item"  data-toggle="tooltip" data-placement="right" title="Setting">
 						<a class="nav-link" id="caknav-settings" data-toggle="tab" href="#cakmenu-settings">
 						<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="svg-main-icon" stroke="currentColor" fill="none">
 								<path d="M15.9497475,3.80761184 L13.0246125,6.73274681 C12.2435639,7.51379539 12.2435639,8.78012535 13.0246125,9.56117394 L14.4388261,10.9753875 C15.2198746,11.7564361 16.4862046,11.7564361 17.2672532,10.9753875 L20.1923882,8.05025253 C20.7341101,10.0447871 20.2295941,12.2556873 18.674559,13.8107223 C16.8453326,15.6399488 14.1085592,16.0155296 11.8839934,14.9444337 L6.75735931,20.0710678 C5.97631073,20.8521164 4.70998077,20.8521164 3.92893219,20.0710678 C3.1478836,19.2900192 3.1478836,18.0236893 3.92893219,17.2426407 L9.05556629,12.1160066 C7.98447038,9.89144078 8.36005124,7.15466739 10.1892777,5.32544095 C11.7443127,3.77040588 13.9552129,3.26588995 15.9497475,3.80761184 Z" fill="#ffffff"/>
