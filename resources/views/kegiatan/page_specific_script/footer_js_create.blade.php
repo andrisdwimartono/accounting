80fc6f89099cbeee708d9b044bd1048389f796ea
Andris Dwi Martono <andrisdwimartono@gmail.com>
2022-02-18 09:20:46 +0700

    Tanggal Pengajuan
    Tanggal pengajuan
    
    andrisdwimartono

--- resources/views/kegiatan/page_specific_script/footer_js_create.blade.php
+++ resources/views/kegiatan/page_specific_script/footer_js_create.blade.php
@@ -515,9 +515,6 @@ $(document).ready(function() {
 
 @if($page_data["page_method_name"] == "Update" || $page_data["page_method_name"] == "View")
 function getdata(){
-    @if($page_data["page_data_urlname"] == "pengajuan")
-    return;
-    @endif
     cto_loading_show();
     $.ajax({
         url: "/getdata{{$page_data["page_data_urlname"]}}",
