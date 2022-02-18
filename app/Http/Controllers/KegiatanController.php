e52322a6d93b93e818f97319d740bcf3345de8d9
Andris Dwi Martono <andrisdwimartono@gmail.com>
2022-02-18 07:37:38 +0700

    Tanggal Pengajuan
    Tanggal pengajuan
    
    andrisdwimartono

--- app/Http/Controllers/KegiatanController.php
+++ app/Http/Controllers/KegiatanController.php
@@ -594,15 +594,17 @@ class KegiatanController extends Controller
                 "status" => "submitting"
             ]);
             
-            foreach(Approvalsetting::where("jenismenu", "pengajuan")->get() as $appr){
-                Approval::create([
-                    "no_seq" => $appr->no_seq,
-                    "parent_id" => $id,
-                    "role"=> $appr->role,
-                    "role_label"=> $appr->role_label,
-                    "jenismenu"=> $appr->jenismenu,
-                    "user_creator_id" => Auth::user()->id
-                ]);
+            if(!$isapproving){
+                foreach(Approvalsetting::where("jenismenu", "pengajuan")->get() as $appr){
+                    Approval::create([
+                        "no_seq" => $appr->no_seq,
+                        "parent_id" => $id,
+                        "role"=> $appr->role,
+                        "role_label"=> $appr->role_label,
+                        "jenismenu"=> $appr->jenismenu,
+                        "user_creator_id" => Auth::user()->id
+                    ]);
+                }
             }
 
             return response()->json([
