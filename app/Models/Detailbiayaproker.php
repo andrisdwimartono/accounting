2ad347222c79e5f4aeb79dad932ddf7cc787df73
Andris Dwi Martono <andrisdwimartono@gmail.com>
2022-02-28 19:27:48 +0700

    Ganti Posisi Volume dan Standard Biaya
    ganti posisi volume dan standar biaya
    
    andrisdwimartono

--- app/Models/Detailbiayaproker.php
+++ app/Models/Detailbiayaproker.php
@@ -9,7 +9,7 @@ class Detailbiayaproker extends Model
 {
     use HasFactory;
     protected $table = 'detailbiayaprokers';
-    protected $fillable = ['parent_id', 'no_seq', 'detailbiayaproker_name', 'deskripsibiaya', 'standarbiaya', 'satuan', 'satuan_label', 'user_creator_id', 'user_updater_id', 'updated_at'];
+    protected $fillable = ['parent_id', 'no_seq', 'detailbiayaproker_name', 'deskripsibiaya', 'standarbiaya', 'volume', 'satuan', 'satuan_label', 'user_creator_id', 'user_updater_id', 'updated_at'];
 
     function getProgramkerja(){
         return $this->hasOne('App\Models\Programkerja');
